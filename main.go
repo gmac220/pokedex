dbd1a49cd5264d8ce5dca1cb630825c3cd497e66
gmac220 <godfreymacasero@gmail.com>
2019-10-10 14:47:41 -0500

    moved line

--- main.go
+++ main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
-	"os"
+	"flag"
+	"net/http"
 	"strconv"
 )
 
 //var pokedex []Pokemon
+var pokedex Pokedex
+
+func addPokemon(res http.ResponseWriter, req *http.Request) {
+	id, _ := strconv.Atoi(req.FormValue("id"))
+	name := req.FormValue("name")
+	var caught bool
+	if req.FormValue("caught") == "true" {
+		caught = true
+	}
+	httpPokemon := NewPokemon(id, name, caught)
+	pokedex.Add(httpPokemon)
+	pokedex.List()
+}
 
 func main() {
-	pokedex := Pokedex{}
+	pokedex = Pokedex{}
 	bulbasaur := NewPokemon(1, "Bulbasaur", true)
 	pokedex.Add(bulbasaur)
 
-	if len(os.Args) > 1 {
-		id, _ := strconv.Atoi(os.Args[1])
-		name := os.Args[2]
-		var caught bool
-		if os.Args[3] == "true" {
-			caught = true
-		} else {
-			caught = false
-		}
+	httpport := flag.String("http", ":8080", "port value for the server")
+	flag.Parse()
 
-		newP := NewPokemon(id, name, caught)
-		pokedex.Add(newP)
-	}
+	http.HandleFunc("/pokemon", addPokemon)
+	http.ListenAndServe(*httpport, nil)
+
+	// if len(os.Args) > 1 {
+	// 	id, _ := strconv.Atoi(os.Args[1])
+	// 	name := os.Args[2]
+	// 	var caught bool
+	// 	if os.Args[3] == "true" {
+	// 		caught = true
+	// 	} else {
+	// 		caught = false
+	// 	}
+
+	// 	newP := NewPokemon(id, name, caught)
+	// 	pokedex.Add(newP)
+	// }
 	pokedex.List()
 
 }
